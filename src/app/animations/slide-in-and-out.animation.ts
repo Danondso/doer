8a1c6def7e2924fbe04bbbe342b0c95ce162f3a3
Dublin Anondson <stratophone2112@gmail.com>
2019-06-15 10:38:27 -0500

    chore: linted

--- src/app/animations/slide-in-and-out.animation.ts
+++ src/app/animations/slide-in-and-out.animation.ts
@@ -3,29 +3,29 @@ import { transition, trigger, style, state, animate, group } from '@angular/anim
 export const SlideInAndOut = [
     trigger('showTaskCreateAnimation', [
         state('true', style({
-            'opacity': '1', 'visibility': 'visible'
+            opacity: '1', visibility: 'visible'
         })),
         state('false', style({
-            'opacity': '0', 'visibility': 'hidden'
+            opacity: '0', visibility: 'hidden'
         })),
         transition('true => false', [group([
             animate('400ms ease-in-out', style({
-                'opacity': '0'
+                opacity: '0'
             })),
             animate('600ms ease-in-out', style({
                 'max-height': '0px'
             })),
             animate('700ms ease-in-out', style({
-                'visibility': 'hidden'
+                visibility: 'hidden'
             }))
         ]
         )]),
         transition('false => true', [group([
             animate('1ms ease-in-out', style({
-                'visibility': 'visible'
+                visibility: 'visible'
             })),
             animate('800ms ease-in-out', style({
-                'opacity': '1'
+                opacity: '1'
             }))
         ]
         )])
